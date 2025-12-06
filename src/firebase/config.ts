7154da7ddfc5cc54c69fb20eb1e5cf52b34509ef
Antonio Rafael Carneiro da Silva <rsetbrasiltecnologia@gmail.com>
2025-12-06 01:14:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6063497560-c6d00",
+  "appId": "1:211373143010:web:f5038b11bd95d2ac5f3fbb",
+  "apiKey": "AIzaSyBwin2N3MDQMTbLFKPeNsVpuupDsSS4neY",
+  "authDomain": "studio-6063497560-c6d00.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "211373143010"
+};
