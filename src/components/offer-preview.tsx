fb61f471726cf03ae21bfec4f1d81a062ffc2332
Antonio Rafael Carneiro da Silva <rsetbrasiltecnologia@gmail.com>
2025-12-06 19:12:21 +0000

    remova esse espaço

--- src/components/offer-preview.tsx
+++ src/components/offer-preview.tsx
@@ -12,7 +12,7 @@ export function OfferPreview({ offer, children }: OfferPreviewProps) {
   return (
     <div
       id="print-area"
-      className="flex w-full aspect-[210/297] bg-white rounded-lg shadow-lg border transition-all duration-300"
+      className="w-full aspect-[210/297] bg-white rounded-lg shadow-lg border transition-all duration-300"
     >
       {children}
     </div>
